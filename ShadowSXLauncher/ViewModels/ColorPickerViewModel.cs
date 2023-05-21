4abd316eac81458260325fb1cd6390059640f777
blazinzzetti <24198652+BlazinZzetti@users.noreply.github.com>
2023-05-21 16:03:03 -0700

    Color Picker Start

---
+++ ShadowSXLauncher/ViewModels/ColorPickerViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Avalonia.Media;
+
+namespace ShadowSXLauncher.ViewModels;
+
+public class ColorPickerViewModel : INotifyPropertyChanged
+{
+    private SolidColorBrush pickedColor = new SolidColorBrush(Colors.White);
+    public SolidColorBrush PickedColor
+    {
+        get
+        {
+            return pickedColor;
+        }
+        set
+        {
+            pickedColor = value;
+            OnPropertyChanged();
+            OnPropertyChanged("PickedColorHexString");
+        }
+    }
+
+    public string PickedColorHexString
+    {
+        get
+        {
+            return pickedColor.Color.R.ToString("X")
+                    + pickedColor.Color.G.ToString("X")
+                    + pickedColor.Color.B.ToString("X");
+        }
+        // set
+        // {
+        //     byte r = byte.Parse((value[0] + value[1]).ToString());
+        //     byte g = byte.Parse((value[2] + value[3]).ToString());
+        //     byte b = byte.Parse((value[4] + value[5]).ToString());
+        //     pickedColor.Color = Color.FromRgb(r, g, b);
+        //     OnPropertyChanged();
+        // }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
