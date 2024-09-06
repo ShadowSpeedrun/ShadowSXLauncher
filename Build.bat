1b592749be5c8a140c77a08c3612ee68bb2e7346
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-09-05 22:43:15 -0700

    Update Build.bat
    Not able to use trim for the install wizard

--- Build.bat
+++ Build.bat
@@ -1,5 +1,3 @@
-dotnet publish --os win --self-contained -p:PublishSingleFile=True -p:PublishTrimmed=True -p:TrimMode=CopyUsed -p:PublishReadyToRun=True
-dotnet publish --os linux --self-contained -p:PublishSingleFile=True -p:PublishTrimmed=True -p:TrimMode=CopyUsed -p:PublishReadyToRun=True
-dotnet publish --os osx --self-contained -p:PublishSingleFile=True -p:PublishTrimmed=True -p:TrimMode=CopyUsed -p:PublishReadyToRun=True
-pause
+dotnet publish --os win --self-contained -p:PublishSingleFile=True -p:TrimMode=CopyUsed -p:PublishReadyToRun=True
+dotnet publish --os linux --self-contained -p:PublishSingleFile=True -p:TrimMode=CopyUsed -p:PublishReadyToRun=True
 start "" "%~dp0bin\Release\net8.0"
